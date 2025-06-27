a551e34cb35812709207f54539eaf818e32ae29d
azuboguko@gmail.com <azuboguko@gmail.com>
2025-06-27 14:06:03 +0100

    Set up server file and code in index.ts file.
    Created a mongoDB collection , users by name, and connected it to the server in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,27 @@ import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// ########## TYPE-SAFE CHECK FOR DB STRING VALUE ##########
+const mongoURLLocal = process.env.MONGO_URL_LOCAL;
+if (!mongoURLLocal) throw new Error("MONGO_URL_LOCAL is not defined");
+
+// ########## CONNECT TO DB ##########
+mongoose.connect(mongoURLLocal);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log("server has started at port", PORT);
+  });
+});
+
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(express.json());
